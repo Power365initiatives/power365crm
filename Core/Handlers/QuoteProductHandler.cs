692eef11c1c94d61d81bc8fde26560319386d301
Ignacio Barrio Santos <ignacio.barrio@neoris.com>
2024-02-12 09:58:32 +0100

    QuoteProduct plugin calculating detail amount in create, update and delete events.

---
+++ Core/Handlers/QuoteProductHandler.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using P365I_CRM.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using static P365I_CRM.Core.Helpers.Common;
+
+namespace P365I_CRM.Core.Handlers
+{
+    public class QuoteProductHandler
+    {
+        private readonly ITracingService _tracingService;
+        private readonly IOrganizationService _service;
+
+        public QuoteProductHandler(ITracingService tracingService, IOrganizationService service)
+        {
+            _tracingService = tracingService;
+            _service = service;
+        }
+
+        public void UpdateQuoteDetailAmount(Entity quoteProduct, string message)
+        {
+            _tracingService.Trace("Start UpdateQuoteDetailAmount");
+
+            Entity quote = new Entity("p365i_quote");
+            if (message == "create")
+            {
+                if (quoteProduct.Contains("p365i_selectproduct"))
+                {
+                    var selectProduct = quoteProduct.GetAttributeValue<Boolean>("p365i_selectproduct");
+                    if (!selectProduct) //false
+                    {
+                        if (quoteProduct.Contains("p365i_existingproduct"))
+                        {
+                            var existingProductRef = quoteProduct.GetAttributeValue<EntityReference>("p365i_existingproduct");
+                            Entity product = _service.Retrieve("p365i_listitem", existingProductRef.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet("p365i_name"));
+                            quoteProduct["p365i_product"] = product.GetAttributeValue<string>("p365i_name");
+                        }
+                        else
+                            quoteProduct["p365i_product"] = "new product";
+                    }
+                    else
+                    {
+                        var writeInProduct = quoteProduct.GetAttributeValue<string>("p365i_writeinproduct");
+                        quoteProduct["p365i_product"] = writeInProduct;
+                    }
+                }
+                else
+                    quoteProduct["p365i_product"] = "new product";
+
+                _service.Update(quoteProduct);
+            }
+            Guid parentId = quoteProduct.GetAttributeValue<EntityReference>("p365i_quote").Id;
+            //calculate Detailed Amount
+            EntityCollection quoteProducts = Common.QuerybyAttribute(_service, _tracingService, "p365i_quoteproduct", new Microsoft.Xrm.Sdk.Query.ColumnSet("p365i_totalcost"), "p365i_quote", parentId.ToString());
+            Decimal detailedAmount = 0;
+            foreach (var qProduct in quoteProducts.Entities)
+            {
+                detailedAmount += qProduct.Attributes.Contains("p365i_totalcost") ? qProduct.GetAttributeValue<Money>("p365i_totalcost").Value : 0;
+            }
+            quote.Id = parentId;
+            quote.Attributes["p365i_detailedamount"] = new Money(detailedAmount);
+            _service.Update(quote);
+            _tracingService.Trace("End UpdateQuoteDetailAmount");
+        }
+
+        public void DeleteQuoteProduct(Guid recordId, IPluginExecutionContext context)
+        {
+            _tracingService.Trace("Start DeleteQuoteProduct");
+            _service.Delete("p365i_quoteproduct", recordId);
+
+            Thread.Sleep(3000);
+            context.OutputParameters["DeleteQuoteProduct_RecordIdDeleted"] = recordId.ToString();
+
+            _tracingService.Trace("End DeleteQuoteProduct");
+        }        
+    }
+}
---
+++ Main/Plugins/QuoteProductPostOp.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using P365I_CRM.Core.Handlers;
+using P365I_CRM.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using static P365I_CRM.Core.Helpers.Common;
+
+namespace P365I_CRM.Sales.Plugins
+{
+    public class QuoteProductPostOp : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = factory.CreateOrganizationService(context.UserId);
+            var message = context.MessageName.ToLower();
+
+            tracingService.Trace("Start QuoteProductPostOp");
+
+            var quoteProductHandler = new QuoteProductHandler(tracingService, service);
+
+            if (message == "create")
+            {
+                if (context.Depth > 1)
+                    return;
+
+                var target = (Entity)context.InputParameters["Target"];
+                quoteProductHandler.UpdateQuoteDetailAmount(target, message);
+            }
+            else if (message == "update")
+            {
+                if (context.Depth > 1)
+                    return;
+
+                var postImage = context.PostEntityImages != null && context.PostEntityImages.Contains("PostImage") ? (Entity)context.PostEntityImages["PostImage"] : new Entity();
+                quoteProductHandler.UpdateQuoteDetailAmount(postImage, string.Empty);
+            }
+            else if (message == "delete")
+            {
+                var preImage = context.PreEntityImages != null && context.PreEntityImages.Contains("PreImage") ? (Entity)context.PreEntityImages["PreImage"] : new Entity();
+                quoteProductHandler.UpdateQuoteDetailAmount(preImage, message);
+            }
+
+            tracingService.Trace("End QuoteProductPostOp");
+        }
+    }
+}
